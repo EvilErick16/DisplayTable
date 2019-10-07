7b94e0c026477aab5093a8189296dbafd38ac8f6
Erick Juarez <juarez.erick16@csu.fullerton.edu>
2019-10-06 22:06:15 -0700

    Display table with Java

--- app/src/main/java/com/csuf/cpsc41102/displaytable/MainActivity.java
+++ app/src/main/java/com/csuf/cpsc41102/displaytable/MainActivity.java
@@ -12,26 +12,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.graphics.Color;
 
+@TargetApi(21)
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Parent method call
         super.onCreate(savedInstanceState);
-
-        /* Steps to create Table:
-        1. create root as a scrollable layout
-        horizontal linear layout (firstLevel)
-            2. add vertical linear layout for numbers (grayLayout)
-            3. add vertical linear layout next to layout on previous step (secondLevel)
-                4. text view object (blackTextView)
-                5. add Grid layout 3x3(whiteGrid)
-                6. grid layout 3x3 (cyanGrid)
-                7. horizontal linear layout (thirdLevel)
-                    8. vertical linear layout (whiteLayout)
-                    9. grid layout (yellowGrid)
-                    10. vertical linear layout (orangeLayout)
-        */
 
         // Create root layout object Scrollview
         HorizontalScrollView scrollableRoot = new HorizontalScrollView(this);
@@ -43,8 +30,12 @@ public class MainActivity extends AppCompatActivity {
         // Create and add grayLayout to firsLevel
         LinearLayout grayLayout = new LinearLayout(this);
         grayLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 0, 0, 1);
         for (int i = 1; i < 12; i++){
             TextView tv = new TextView(this);
+            tv.setLayoutParams(lp);
             tv.setText("" + i);
             tv.setTextSize(12);
             tv.setBackgroundColor(Color.LTGRAY);
@@ -62,105 +53,136 @@ public class MainActivity extends AppCompatActivity {
         blackTextView.setTextColor(Color.WHITE);
         blackTextView.setTextSize(12);
         blackTextView.setBackgroundColor(Color.BLACK);
-        blackTextView.setPadding(400, 0, 400, 0);
+        blackTextView.setGravity(Gravity.CENTER_HORIZONTAL);
         secondLevel.addView(blackTextView);
 
-        // Create whiteGrid and add it to second level
-        GridLayout whiteGrid = new GridLayout(this);
-        whiteGrid.setColumnCount(3);
-        whiteGrid.setRowCount(2);
-        whiteGrid.setBackgroundColor(Color.WHITE);
-        createWhiteGrid(whiteGrid, "properties", 0, 0);
-        createWhiteGrid(whiteGrid, "name", 1, 0);
-        createWhiteGrid(whiteGrid, "category", 1, 1);
-        createWhiteGrid(whiteGrid, "Day Hour Classification", 2, 0);
-        createWhiteGrid(whiteGrid, "Day and Time", 2, 1);
-        secondLevel.addView(whiteGrid);
+        // Create grid and add it to second level
+        GridLayout bigGrid = new GridLayout(this);
+        bigGrid.setColumnCount(4);
+        bigGrid.setRowCount(10);
+        bigGrid.setBackgroundColor(Color.BLACK);
 
-        // Create cyanGrid ans add it to second level
-        GridLayout cyanGrid = new GridLayout(this);
-        cyanGrid.setColumnCount(4);
-        cyanGrid.setRowCount(3);
-        cyanGrid.setBackgroundColor(Color.CYAN);
-        createCyanGrid(cyanGrid, "Rule", 0, 0);
-        createCyanGrid(cyanGrid, "", 0, 1);
-        createCyanGrid(cyanGrid, "", 0, 2);
-        createCyanGrid(cyanGrid, "C1", 1, 0);
-        createCyanGrid(cyanGrid, "Min <= hour && hour <= max", 1, 1);
-        createCyanGrid(cyanGrid, "int min", 1, 2);
-        createCyanGrid(cyanGrid, "int max", 2, 2);
-        createCyanGrid(cyanGrid, "A1", 3, 0);
-        createCyanGrid(cyanGrid, "System.out.println(greeting + \", World!\"", 3, 1);
-        createCyanGrid(cyanGrid, "String greeting", 3, 2);
-        secondLevel.addView(cyanGrid);
+        // Add content to grid Row 0
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(0, GridLayout.CENTER, 1f),
+                GridLayout.spec(0, GridLayout.CENTER, 2f), "properties");
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(1, GridLayout.CENTER, 15f),
+                GridLayout.spec(0, 1f), "name");
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(2, GridLayout.CENTER, 1f),
+                GridLayout.spec(0, GridLayout.CENTER, 2f), "");
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(3, GridLayout.LEFT, 1f),
+                GridLayout.spec(0, 1f), "Day Hour Classification");
 
+        // Add content to grid Row 1
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(0, GridLayout.CENTER, 1f),
+                GridLayout.spec(1, GridLayout.CENTER, 2f), "");
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(1, GridLayout.CENTER, 15f),
+                GridLayout.spec(1, 1f), "category");
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(2, GridLayout.CENTER, 1f),
+                GridLayout.spec(1, GridLayout.CENTER, 2f), "");
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(3, GridLayout.LEFT, 1f),
+                GridLayout.spec(1, 1f), "Day and Time");
 
+        // Add content to grid Row 2
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(0, GridLayout.CENTER, 1f),
+                GridLayout.spec(2, 1f), "Rule");
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(1, GridLayout.CENTER, 15f),
+                GridLayout.spec(2, 1f), "C1");
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(2, GridLayout.CENTER, 1f),
+                GridLayout.spec(2, 1f), "");
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(3, GridLayout.CENTER, 1f),
+                GridLayout.spec(2, 1f), "A1");
 
+        // Add content to grid Row 3
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(0, GridLayout.CENTER, 1f),
+                GridLayout.spec(3, 1f), "");
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(1, GridLayout.CENTER, 15f),
+                GridLayout.spec(3, 1f), "min <= hour && hour <= max");
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(2, GridLayout.CENTER, 1f),
+                GridLayout.spec(3, 1f), "");
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(3, GridLayout.CENTER, 1f),
+                GridLayout.spec(3, 1f), "System.out.println(greeting + \" , World!\")");
 
+        // Add content to grid Row 4
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(0, GridLayout.CENTER, 1f),
+                GridLayout.spec(4, 1f), "");
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(1, GridLayout.CENTER, 1f),
+                GridLayout.spec(4, 1f), "int min");
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(2, GridLayout.CENTER, 1f),
+                GridLayout.spec(4, 1f), "int max");
+        createGrid(bigGrid, Color.CYAN, GridLayout.spec(3, GridLayout.CENTER, 1f),
+                GridLayout.spec(4, 1f), "String greeting");
 
+        // Add content to grid Row 5
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(0, GridLayout.LEFT, 1f),
+                GridLayout.spec(5, 1f), "Rule");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(1, GridLayout.LEFT, 1f),
+                GridLayout.spec(5, 1f), "From");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(2, GridLayout.LEFT, 1f),
+                GridLayout.spec(5, 1f), "To");
+        createGrid(bigGrid, Color.RED, GridLayout.spec(3, GridLayout.LEFT, 1f),
+                GridLayout.spec(5, 1f), "Greeting");
+
+        // Add content to grid Row 6
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(0, GridLayout.LEFT, 1f),
+                GridLayout.spec(6, 1f), "R10");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(1, GridLayout.RIGHT, 1f),
+                GridLayout.spec(6, 1f), "0");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(2, GridLayout.RIGHT, 1f),
+                GridLayout.spec(6, 1f), "11");
+        createGrid(bigGrid, Color.RED, GridLayout.spec(3, GridLayout.LEFT, 1f),
+                GridLayout.spec(6, 1f), "Good Morning");
+
+        // Add content to grid Row 7
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(0, GridLayout.LEFT, 1f),
+                GridLayout.spec(7, 1f), "R20");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(1, GridLayout.RIGHT, 1f),
+                GridLayout.spec(7, 1f), "12");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(2, GridLayout.RIGHT, 1f),
+                GridLayout.spec(7, 1f), "17");
+        createGrid(bigGrid, Color.RED, GridLayout.spec(3, GridLayout.LEFT, 1f),
+                GridLayout.spec(7, 1f), "Good Afternoon");
+
+        // Add content to grid Row 8
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(0, GridLayout.LEFT, 1f),
+                GridLayout.spec(8, 1f), "R30");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(1, GridLayout.RIGHT, 1f),
+                GridLayout.spec(8, 1f), "18");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(2, GridLayout.RIGHT, 1f),
+                GridLayout.spec(8, 1f), "21");
+        createGrid(bigGrid, Color.RED, GridLayout.spec(3, GridLayout.LEFT, 1f),
+                GridLayout.spec(8, 1f), "Good Evening");
+
+        // Add content to grid Row 9
+        createGrid(bigGrid, Color.WHITE, GridLayout.spec(0, GridLayout.LEFT, 1f),
+                GridLayout.spec(9, 1f), "R40");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(1, GridLayout.RIGHT, 1f),
+                GridLayout.spec(9, 1f), "22");
+        createGrid(bigGrid, Color.YELLOW, GridLayout.spec(2, GridLayout.RIGHT,1f),
+                GridLayout.spec(9, 1f), "23");
+        createGrid(bigGrid, Color.RED, GridLayout.spec(3, GridLayout.LEFT, 1f),
+                GridLayout.spec(9, 1f), "Good Night");
+
+        // Add content view
+        secondLevel.addView(bigGrid);
         firstLevel.addView(secondLevel);
-
-
         scrollableRoot.addView(firstLevel);
-        // Display root object
         setContentView(scrollableRoot);
     }
 
     @TargetApi(21)
-    private void createWhiteGrid(GridLayout l, String s, int col, int row){
-        GridLayout.Spec colSpec;
-        GridLayout.Spec rowSpec;
-
-        if (col == 0 && row == 0){
-            colSpec = GridLayout.spec(col, GridLayout.CENTER,1f);
-            rowSpec = GridLayout.spec(row,GridLayout.CENTER, 1f);
-        }
-        else if (col == 1){
-            colSpec = GridLayout.spec(col, GridLayout.CENTER, 1f);
-            rowSpec = GridLayout.spec(row);
-        }
-        else {
-            colSpec = GridLayout.spec(col, GridLayout.LEFT, 1f);
-            rowSpec = GridLayout.spec(row);
-        }
-
+    private void createGrid(GridLayout layout, int bgColor, GridLayout.Spec colSpec, GridLayout.Spec rowSpec, String text){
+        // create column and row spec object
         GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpec, colSpec);
-        //params.setGravity(Gravity.CENTER_HORIZONTAL);
+        params.width = 425;
+
+        params.setMargins(0, 0, 1, 1);
 
         // Create text view object
         TextView tv = new TextView(this);
-        tv.setText(s);
-        tv.setTextSize(12);
-
         tv.setLayoutParams(params);
-        l.addView(tv);
-    }
-
-    @TargetApi(21)
-    private void createCyanGrid(GridLayout l, String s, int col, int row){
-        GridLayout.Spec colSpec;
-        GridLayout.Spec rowSpec;
-
-        if (col == 0 || col == 3) {
-            colSpec = GridLayout.spec(col, GridLayout.CENTER, 1f);
-            rowSpec = GridLayout.spec(row, GridLayout.CENTER);
-        }
-        else if (col == 1 && (row == 0 || row == 1)){
-            colSpec = GridLayout.spec(col, GridLayout.CENTER, 1f);
-            rowSpec = GridLayout.spec(row, GridLayout.CENTER);
-        }
-        else {
-            colSpec = GridLayout.spec(col, GridLayout.CENTER);
-            rowSpec = GridLayout.spec(row, GridLayout.CENTER);
-        }
-        GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpec, colSpec);
-
-        // Create text view object
-        TextView tv = new TextView(this);
-        tv.setText(s);
+        tv.setText(text);
         tv.setTextSize(12);
-
-        tv.setLayoutParams(params);
-        l.addView(tv);
+        tv.setBackgroundColor(bgColor);
+        layout.addView(tv);
     }
 }
